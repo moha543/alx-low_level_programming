398d8e1de60a01af0d082fcec10788b3d4b82fdf
moha543 <mohamedabukar412@gmail.com>
2022-09-08 11:48:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */ 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful \" - Dora korpar, 2015-10-9\n", 59);
+	write(2, "and that piece of art is usefull\" -Dora korpar, 2015-10-9\n", 61);
 	return (1);
 }
