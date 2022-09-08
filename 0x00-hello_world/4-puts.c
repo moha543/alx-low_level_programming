aa56efada60bbb9d5b8ec28458f494f49aabea90
moha543 <mohamedabukar412@gmail.com>
2022-09-08 07:48:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
   int main(void)
   
   {
-	  puts("\"Programming is like building a multiligual puzzle");
+	  puts("\"Programming is like building a multilingual puzzle");
 	  return (0);
-
 
   }
 		 
