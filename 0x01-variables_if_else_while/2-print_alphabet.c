ba2cb5628ed37412eb38af5140a65d75b8a2754b
moha543 <mohamedabukar412@gmail.com>
2022-09-09 13:39:15 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	char low;
+	char ch;
 
-	for (low = 'a'; low <= 'z' low++)
-		putchar(low);
-	putchar('\n';
+	for (ch = 'a' ; ch <= 'z' ;ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 
 	return(0);
 }
