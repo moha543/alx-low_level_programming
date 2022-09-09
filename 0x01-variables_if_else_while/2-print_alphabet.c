be50561f4be291d884f36a701c43e1cbd566b16a
moha543 <mohamedabukar412@gmail.com>
2022-09-09 13:52:37 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /**
- * main - Entry point 
+ *main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
@@ -11,7 +12,7 @@ int main(void)
 {
 	char ch;
 
-	for ( ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
