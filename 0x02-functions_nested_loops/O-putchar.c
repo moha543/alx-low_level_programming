24cd233e7716282f7cfd06c2afb220d5d7c473ee
moha543 <mohamedabukar412@gmail.com>
2022-09-12 12:47:00 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/O-putchar.c
@@ -0,0 +1,19 @@
+#include "0-putchar.c"
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char *sh = "0-putchar.c";
+
+	while (*sh)
+	{
+		_putchar(*sh); sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
