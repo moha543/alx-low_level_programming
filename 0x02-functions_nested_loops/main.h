07c7bc7cd63cdd9fe9cffe068ba8ed82573d39f0
moha543 <mohamedabukar412@gmail.com>
2022-09-13 10:41:43 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char name[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(name[i]);
+	}
+	_putchar(10);
+	return (0);
+}
