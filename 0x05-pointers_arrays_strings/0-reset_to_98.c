11cdf682e2d8cedcc4b1170368da64763827054c
moha543 <mohamedabukar412@gmail.com>
2022-09-21 14:09:20 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,12 +4,12 @@
  * rest_to_98 - Direct a pointer to an int and changes
  * and changes the value to 98
  *
- * @n: pointing to the integer
+ * @n:The variable pointing to the integer
  *
  * Return: Nothing 
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
